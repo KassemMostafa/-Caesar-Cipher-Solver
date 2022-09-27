4aa093e9fea51d8ffbb43c9d52f3db3ec481d650
KassemMostafa <mostafa.akassem03@gmail.com>
2022-09-27 10:41:31 +0200

    Removed unecessary function

--- cesar.py
+++ cesar.py
@@ -4,15 +4,7 @@ txt1 = "NWLAHYCXPAJYQRNUNLQROOANVNWCYJAMNLJUJPNJDBBRLXWWDLXVVNUNLQROOANMNLNBJAXD
 txt2 = "vcfgrwqwfsbhfsntowbsobgfsbhfsnqvsnjcigsghqsoixcifrviwtshseicwbsgojsnjcigdogeisjcigoihfsgofhwgobgjcigbsrsjsnqwfqizsfrobgzsgfisgzsgxcifgcijfopzsgeiojsqzsggwubsgrsjchfsdfctsggwcbdofzseiszsghhcbashwsf"
 alphabet = string.ascii_uppercase
 
-# def stringtolist(s1):
-#     list=[]
-#     list[:0]=s1
-#     return s1
-
-def breakcesaradroite(s1):
-    # l1=[]
-    # l1[:0] = str
-    # l1 = stringtolist(str)
+def breakcaesar(s1):
     s1 = s1.upper()
     decrypted = ""
     maxchar = max(s1,key=s1.count)
@@ -26,30 +18,9 @@ def breakcesaradroite(s1):
         decrypted += chr(k)
     return decrypted
 
-def breakcesaragauche(s1):
-    # l1=[]
-    # l1[:0] = str
-    # l1 = stringtolist(str)
-    s1 = s1.upper()
-    decrypted = ""
-    maxchar = max(s1,key=s1.count)
-    print(maxchar)
-    n = ord(maxchar) #valeur ascii de la lettre la plus fréquente
-    n = abs(n-ord("E")) #decalage
-
-    for c in s1:
-        k = ord(c) + n
-        if (k > 90):
-            k -= 26       
-        decrypted += chr(k)
-    return decrypted
 
 
-print("Text 1 Décrypté -> : ")
-print(breakcesaradroite(txt1))
-print("Text 1 Décrypté <- : ")
-print(breakcesaragauche(txt1))
-print("Text 2 Décrypté ->: ")
-print(breakcesaradroite(txt2))
-print("Text 2 Décrypté <-: ")
-print(breakcesaragauche(txt2))
+print("Text 1 Décrypté : ")
+print(breakcaesar(txt1))
+print("Text 2 Décrypté : ")
+print(breakcaesar(txt2))
